cef492f096bbcc0a248733932d72b4421c481317
David Horstman <davidzilla27@gmail.com>
2020-05-28 18:30:31 -0400

    Revert to simpler implementation

--- data/data-model.js
+++ data/data-model.js
@@ -12,20 +12,11 @@ function getShoppingList(id) {
   return db("");
 }
 
-// SELECT s.step_number, s.directions, r.name AS recipe_name
-//  FROM steps AS s
-//  JOIN recipes AS r
-//  ON r.id = s.recipe_id
-//  WHERE recipe_id=1 ORDER BY step_number;
+// SELECT * from steps
+//   WHERE recipe_id=1
+//   ORDER BY step_number;
 function getInstructions(recipe_id) {
-  return db("steps")
-    .where({ recipe_id })
-    .join("recipes", "steps.recipe_id", "recipes.id")
-    .select(
-      "steps.step_number",
-      "steps.directions",
-      "recipes.name as recipe_name"
-    );
+  return db("steps").where({ recipe_id }).select("step_number","directions");
 }
 
 module.exports = { getRecipes, getRecipe, getShoppingList, getInstructions };

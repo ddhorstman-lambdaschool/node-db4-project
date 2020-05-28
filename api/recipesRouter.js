57585db5b271ea745bdfab3826621edb7ee39879
David Horstman <davidzilla27@gmail.com>
2020-05-28 19:35:07 -0400

    Went nuts but made shopping list beautifully formatted

--- api/recipesRouter.js
+++ api/recipesRouter.js
@@ -17,9 +17,76 @@ router.get(
   "/:id/instructions",
   validateID,
   catchAsync(async (req, res) => {
-    const {id} = req.recipe;
+    const { id } = req.recipe;
     const instructions = await db.getInstructions(id);
-    res.status(200).json(instructions)
+    res.status(200).json(instructions);
+  })
+);
+
+router.get(
+  "/:id/shoppingList",
+  validateID,
+  catchAsync(async (req, res) => {
+    const { id } = req.recipe;
+    const shoppingList = await db
+      .getShoppingList(id)
+      .then(list =>
+        list.map(({ name, quantity, unit }) => ({
+          name,
+          amount: { quantity, unit },
+        }))
+      )
+      .then(list =>
+        list.reduce((ac, val) => {
+          const existingEntry = ac.find(item => item.name === val.name);
+          if (existingEntry) {
+            Array.isArray(existingEntry.amount)
+              ? existingEntry.amount.push(val.amount)
+              : (existingEntry.amount = [existingEntry.amount, val.amount]);
+          } else ac.push(val);
+          return ac;
+        }, [])
+      )
+      .then(x => {
+        console.log(x);
+        return x;
+      })
+      .then(list =>
+        list.map(item =>
+          !Array.isArray(item.amount)
+            ? {
+                ...item,
+                amount: `${item.amount.quantity}${
+                  item.amount.unit ? " " + item.amount.unit : ""
+                }${
+                  item.amount.unit ? (item.amount.quantity == 1 ? "" : "s") : ""
+                }`,
+              }
+            : {
+                ...item,
+                amount: item.amount
+                  .reduce((ac, val) => {
+                    const existingUnit = ac.find(
+                      x => x.unit && x.unit === val.unit
+                    );
+                    existingUnit
+                      ? (existingUnit.quantity =
+                          Number(existingUnit.quantity) + Number(val.quantity))
+                      : ac.push(val);
+                    return ac;
+                  }, [])
+                  .reduce(
+                    (ac, amount) =>
+                      ac +
+                      `${ac === "" ? "" : " +"} ${amount.quantity}${
+                        amount.unit ? " " + amount.unit : ""
+                      }${amount.quantity != 1 ? "s" : ""}`,
+                    ""
+                  ),
+              }
+        )
+      );
+    res.status(200).json(shoppingList);
   })
 );
 
